3dc209f75da6a32cbead22b54c503ec96e652155
Evan Ferreira <evanjfer@gmail.com>
2024-09-28 08:09:42 -0400

    Added Backend Structure and Folders and Organization

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,12 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, Request, Response } from 'express';
 
 const app: Express = express();
 const PORT: number = 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("hello world");
+app.get('/', (req: Request, res: Response) => {
+    res.send('hello world');
 });
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+    console.log(`App listening on port ${PORT}`);
 });
