fd4a3707bf9bb200fc2ff100ecb52f50bcdd6fd9
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-09-27 22:55:13 -0400

    install packages and setup typescript

---
+++ backend/src/app.ts
@@ -0,0 +1,12 @@
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+const PORT: number = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("hello world");
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
