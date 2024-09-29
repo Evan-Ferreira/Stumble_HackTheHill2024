3243311db0ba5889298649997a3c31c5b32f0aec
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-09-28 22:56:55 -0400

    change directory to __dirname

--- backend/src/controllers/textToSpeech.ts
+++ backend/src/controllers/textToSpeech.ts
@@ -1,42 +1,41 @@
-import { ElevenLabsClient } from 'elevenlabs';
-import dotenv from 'dotenv';
-import { createWriteStream } from 'fs';
-import { v4 as uuidv4 } from 'uuid';
-import path from 'path';
+import { ElevenLabsClient } from "elevenlabs";
+import dotenv from "dotenv";
+import { createWriteStream } from "fs";
+import { v4 as uuidv4 } from "uuid";
+import path from "path";
 
 dotenv.config();
 
 const client = new ElevenLabsClient({
-    apiKey: process.env.ELEVENLABS_API_KEY,
+  apiKey: process.env.ELEVENLABS_API_KEY,
 });
 
 const addDash = (text: string) => {
-    return text.replace(/\.\s*/g, '. ----- ');
+  return text.replace(/\.\s*/g, ". ----- ");
 };
 
 const textToSpeech = async (text: string, voice: string): Promise<string> => {
-    return new Promise<string>(async (resolve, reject) => {
-        const input = addDash(text);
-        const directory =
-            '/Users/evanferreira/Documents/GitHub/Stumble_HackTheHill2024/backend/audio';
-        const fileName = uuidv4() + '.mp3';
-        const filePath = path.join(directory, fileName);
-        try {
-            const audio = await client.generate({
-                voice: voice,
-                model_id: 'eleven_turbo_v2',
-                text: input,
-            });
-            const fileStream = createWriteStream(filePath);
-            audio.pipe(fileStream);
-            fileStream.on('finish', () => {
-                resolve(filePath);
-            });
-            fileStream.on('error', reject);
-        } catch (error) {
-            reject(error);
-        }
-    });
+  return new Promise<string>(async (resolve, reject) => {
+    const input = addDash(text);
+    const directory = `${__dirname}/../../audio`;
+    const fileName = uuidv4() + ".mp3";
+    const filePath = path.join(directory, fileName);
+    try {
+      const audio = await client.generate({
+        voice: voice,
+        model_id: "eleven_turbo_v2",
+        text: input,
+      });
+      const fileStream = createWriteStream(filePath);
+      audio.pipe(fileStream);
+      fileStream.on("finish", () => {
+        resolve(filePath);
+      });
+      fileStream.on("error", reject);
+    } catch (error) {
+      reject(error);
+    }
+  });
 };
 
 export default textToSpeech;
