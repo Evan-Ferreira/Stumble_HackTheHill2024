15737f87674eed9dffa04721a44816b7b02db0f0
Joseph Liao <100002111+JosephLiao542211@users.noreply.github.com>
2024-09-28 20:27:51 -0400

    menu fix

--- Stumble/src/pages/ModulePage.tsx
+++ Stumble/src/pages/ModulePage.tsx
@@ -12,7 +12,7 @@ const ModulePage: React.FC<ModulePageProps> = ({ index }) => {
   let content = moduleDescriptions[index - 1];
   return (
     <div className="flex h-screen flex-col bg-[#1a1a1a] ">
-      <div className="h-14 w-screen"></div>
+      <div className="h-[8%] w-screen"></div>
 
       <div className="flex flex-grow p-4  ">
         {/* Problem Description */}
